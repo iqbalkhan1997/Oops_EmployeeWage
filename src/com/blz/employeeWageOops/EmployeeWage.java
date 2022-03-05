5c414064198d6f656ce5901dfedb661604a9914b
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-05 22:20:57 +0530

    Added:Employee wage for multiple companies fuctionality

--- src/com/blz/employeeWageOops/EmployeeWage.java
+++ src/com/blz/employeeWageOops/EmployeeWage.java
@@ -3,26 +3,19 @@ package com.blz.employeeWageOops;
 public class EmployeeWage {
     public static final int FULL_TIME=1;
     public static final int PART_TIME=2;
-    public static final int EMP_WAGE_PER_HR=20;
-    public static final int MAX_WORKING_DAYS=20;
-    public static final int MAX_WORKING_HRS=100;
-    public static final int PROBLEMATIC_SCENARIO=36;
+//    public static final int EMP_WAGE_PER_HR=20;
+//    public static final int MAX_WORKING_DAYS=20;
+//    public static final int MAX_WORKING_HRS=100;
+//    public static final int PROBLEMATIC_SCENARIO=36;
 
-    public static int computeEmpWage() {
+    public static void computeEmpWage(String company, int EMP_WAGE_PER_HR, int MAX_WORKING_DAYS, int MAX_WORKING_HRS) {
         int empHr=0;
         int attendance;
         int totalWorkingDays=0,totalEmpHrs=0,totalEmpWage=0;
-        System.out.println("Welcome to Employee Wage Computation Program");
 
-        while(totalEmpHrs < MAX_WORKING_HRS && totalWorkingDays < MAX_WORKING_DAYS)
+        while(totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < MAX_WORKING_DAYS)
         {
-            if (totalEmpHrs==PROBLEMATIC_SCENARIO) {
-                attendance = PART_TIME;
-            }
-            else {
                 attendance = (int) (Math.random() * 10) % 3;
-            }
-
             switch (attendance) {
                 case FULL_TIME:
                     empHr = 8;
@@ -38,11 +31,12 @@ public class EmployeeWage {
         }
         totalEmpWage = EMP_WAGE_PER_HR * totalEmpHrs;
         System.out.println("Employee worked for "+totalWorkingDays+" days "+totalEmpHrs+" hours");
-        System.out.println("Employee's Monthly wage is " + totalEmpWage);
-        return totalEmpWage;
+        System.out.println("For the company "+company+", Employee's Monthly wage is " + totalEmpWage);
     }
     public static void main(String[] args){
-        // EmployeeWage EmployeeWage=new EmployeeWage();
-         computeEmpWage();
+        System.out.println("Welcome to Employee Wage Computation Program");
+        computeEmpWage("Saffola",20,20,100);
+        computeEmpWage("Fortune",15,15,85);
+
     }
 }
