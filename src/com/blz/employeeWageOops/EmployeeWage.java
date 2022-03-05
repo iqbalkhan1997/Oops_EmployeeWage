dd35fedabdd71939f0ef855195f60a13c213c676
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-05 19:11:58 +0530

    Added: Welcome Message

---
+++ src/com/blz/employeeWageOops/EmployeeWage.java
@@ -0,0 +1,8 @@
+package com.blz.employeeWageOops;
+
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage program");
+    }
+}
