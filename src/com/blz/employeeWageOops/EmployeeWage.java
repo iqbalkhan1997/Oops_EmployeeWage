baff9643bdbbcd7bded6ad36bd54fef5c05452c6
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-05 19:35:56 +0530

    Added:wage per month functionality

--- src/com/blz/employeeWageOops/EmployeeWage.java
+++ src/com/blz/employeeWageOops/EmployeeWage.java
@@ -4,28 +4,30 @@ public class EmployeeWage {
     public static final int FULL_TIME=1;
     public static final int PART_TIME=2;
     public static final int EMP_WAGE_PER_HR=20;
+    public static final int MAX_WORKING_DAYS=20;
 
     public static void main(String[] args) {
         int empHr=0;
         int empWage;
+        int totalWorkingDays; int totalEmpWage=0;
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        int attendance=(int)(Math.random()*10) % 3;
+        for(totalWorkingDays=1;totalWorkingDays<=MAX_WORKING_DAYS;totalWorkingDays++) {
+            int Empcheck = (int) (Math.random() * 10) % 3;
 
-        switch (attendance){
-            case FULL_TIME:
-                System.out.println("Employee is working Full Time");
-                empHr=8;
-                break;
-            case PART_TIME:
-                System.out.println("Employee is working Part Time");
-                empHr=4;
-                break;
-            default:
-                System.out.println("Employee is absent");
+            switch (Empcheck) {
+                case FULL_TIME:
+                    empHr = 8;
+                    break;
+                case PART_TIME:
+                    empHr = 4;
+                    break;
+                default:
+            }
+
+            empWage = empHr * EMP_WAGE_PER_HR;
+            totalEmpWage =+ empWage+totalEmpWage;
         }
-
-        empWage=empHr*EMP_WAGE_PER_HR;
-        System.out.println("Employee's wage is " + empWage);
+        System.out.println("Employee's Monthly wage is " + totalEmpWage);
     }
 }
