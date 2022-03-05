6cd30e31938005c108323bb1d8380487123d91bb
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-05 19:43:58 +0530

    Added:MaxWorkingHrs And MaxWorkingDays In Month functionality

--- src/com/blz/employeeWageOops/EmployeeWage.java
+++ src/com/blz/employeeWageOops/EmployeeWage.java
@@ -5,17 +5,25 @@ public class EmployeeWage {
     public static final int PART_TIME=2;
     public static final int EMP_WAGE_PER_HR=20;
     public static final int MAX_WORKING_DAYS=20;
+    public static final int MAX_WORKING_HRS=100;
+    public static final int PROBLEMATIC_SCENARIO=36;
 
     public static void main(String[] args) {
         int empHr=0;
-        int empWage;
-        int totalWorkingDays; int totalEmpWage=0;
+        int attendance;
+        int totalWorkingDays=0,totalEmpHrs=0,totalEmpWage=0;
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        for(totalWorkingDays=1;totalWorkingDays<=MAX_WORKING_DAYS;totalWorkingDays++) {
-            int Empcheck = (int) (Math.random() * 10) % 3;
+        while(totalEmpHrs < MAX_WORKING_HRS && totalWorkingDays < MAX_WORKING_DAYS)
+        {
+            if (totalEmpHrs==PROBLEMATIC_SCENARIO) {
+                attendance = PART_TIME;
+            }
+            else {
+                attendance = (int) (Math.random() * 10) % 3;
+            }
 
-            switch (Empcheck) {
+            switch (attendance) {
                 case FULL_TIME:
                     empHr = 8;
                     break;
@@ -23,11 +31,13 @@ public class EmployeeWage {
                     empHr = 4;
                     break;
                 default:
+                    empHr=0;
             }
-
-            empWage = empHr * EMP_WAGE_PER_HR;
-            totalEmpWage =+ empWage+totalEmpWage;
+            totalWorkingDays++;
+            totalEmpHrs =+ empHr + totalEmpHrs;
         }
+        totalEmpWage = EMP_WAGE_PER_HR * totalEmpHrs;
+        System.out.println("Employee worked for "+totalWorkingDays+" days "+totalEmpHrs+" hours");
         System.out.println("Employee's Monthly wage is " + totalEmpWage);
     }
 }
