a761451a4140aa2cebf75b3bc3afd5bd2f9bf64d
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-05 19:23:30 +0530

    Added:Daily wage functionality

--- src/com/blz/employeeWageOops/EmployeeWage.java
+++ src/com/blz/employeeWageOops/EmployeeWage.java
@@ -1,17 +1,24 @@
 package com.blz.employeeWageOops;
 
 public class EmployeeWage {
+    public static final int FULL_TIME=1;
+    public static final int EMP_WAGE_PER_HR=20;
 
     public static void main(String[] args) {
+        int empHr=0;
+        int empWage;
         System.out.println("Welcome to Employee Wage Computation Program");
-        int Present=1;
+
         int attendance=(int)(Math.random()*10) % 2;
 
-        if(attendance==Present){
+        if(attendance==FULL_TIME){
+            empHr=8;
             System.out.println("Employee is present");
         }
         else{
             System.out.println("Employee is Absent");
         }
+        empWage=empHr*EMP_WAGE_PER_HR;
+        System.out.println("Employee's wage is " + empWage);
     }
 }
