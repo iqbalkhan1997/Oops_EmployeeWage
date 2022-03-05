28c723e91588d59b409b232908e458349f3e6656
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-05 19:17:50 +0530

    Added: attendance checking functionality

--- src/com/blz/employeeWageOops/EmployeeWage.java
+++ src/com/blz/employeeWageOops/EmployeeWage.java
@@ -3,6 +3,15 @@ package com.blz.employeeWageOops;
 public class EmployeeWage {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to employee wage program");
+        System.out.println("Welcome to Employee Wage Computation Program");
+        int Present=1;
+        int attendance=(int)(Math.random()*10) % 2;
+
+        if(attendance==Present){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
     }
 }
